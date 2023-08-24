a08c6584f652ad0a183351ca56511f956a04ff2c
Sihyun3 <yellownyou@gmail.com>
2023-08-25 02:42:32 +0900

    자바,도커 파일 작성

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+RUN microdnf install findutils
+RUN mkdir /project
+WORKDIR /project
+COPY ./ .
+RUN ./gradlew build
+WORKDIR build/libs
+#RUN chmod +x *.jar
+ENTRYPOINT	 ["java","-jar","*.jar"]
---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@GetMapping("/")
+	public ResponseEntity<String> Main() throws Exception {
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			String strIpAdress = inetAddress.getHostAddress();
+			String html = " 현재 내부 아이피 주소는 <b>" + strIpAdress+"</b> 입니다.";
+			return ResponseEntity.status(HttpStatus.OK).body(html);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("아이피 주소를 불러 올 수 없습니다");
+		}
+			
+	}
+
+}
